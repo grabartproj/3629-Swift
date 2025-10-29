89bbc4a5050e94420d52a9d8ff066181bea1aa3a
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-30 00:24:43 +0200

    save

---
+++ Cluckocalypse/App/CluckocalypseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluckocalypseApp.swift
+//  Cluckocalypse
+//
+//  Created by Serhii Babchuk on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CluckocalypseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CALaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Cluckocalypse/Pods-Cluckocalypse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cluckocalypse : NSObject
+@end
+@implementation PodsDummy_Pods_Cluckocalypse
+@end
